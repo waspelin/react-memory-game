b0914800de4e06645ffd6edb12ec27a538752c3b
waspelin <waspelin@gmail.com>
2018-05-04 12:36:14 -0700

    updating app

---
+++ memory-game/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Memory">
+        1
+        2
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+      </div>
+    );
+  }
+}
+
+export default App;
